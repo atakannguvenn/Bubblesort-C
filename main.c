e0c05e2b2ee79cf9a61274603d99cc99f4322a22
Atakan Guven <50146105+atakannguvenn@users.noreply.github.com>
2019-05-02 00:27:00 +0800

    Update main.c

--- main.c
+++ main.c
@@ -6,8 +6,8 @@ void swap(int a,int z,char *sentence);
 int main()
 {
 	int i=0,j=0;
-    char sentence[20]="ebrzaisnic";
-    swap(i,j,sentence);
+        char sentence[20]="ebrzaisnic";
+        swap(i,j,sentence);
 	printf("Origional : %s\n\n",sentence);
 	printf("\n\n Final Result :    %s\n\n",sentence);
 	system("pause");
@@ -16,21 +16,21 @@ int main()
 
 void swap(int a,int z,char *sentence)
 {
-    int k=0;
-    char tmp;
-    for(z=0;sentence[z]!='\0';z=z+1);
+    	int k=0;
+    	char tmp;
+    	for(z=0;sentence[z]!='\0';z=z+1);
 	z=z-1;
 	while(z > k)
 	{
 		while(z>a)
-    	{
+    		{
 			if(sentence[a] > sentence[a+1])
-         	{
+         		{
 				tmp = sentence[a];
-              	sentence[a] = sentence[a+1];
-              	sentence[a+1] = tmp;
-         	}
-         	a = a+1;
+              			sentence[a] = sentence[a+1];
+              			sentence[a+1] = tmp;
+         		}
+         		a = a+1;
  		}
 		printf("In Progress :    %s\n",sentence);
 		a = 0;
